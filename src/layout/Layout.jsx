3bb2f9e3882e6e698f64b05c5cdb3fdccc4cf182
unknown <ulanuulunursultan36@gmail.com>
2023-10-12 11:50:56 +0600

    bla bla

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import Header from './header/Header'
 import Footer from './footer/Footer'
-import Banner from '../components/baner/Banner'
-import { Routes, Route, Outlet } from 'react-router-dom'
-import Features from '../components/features/Features'
-import Stock from '../components/stock/Stock'
-import Holidays from '../components/holidays/Holidays'
-import Popular from '../components/popular/Popular'
-import News from '../components/news/News'
-import Care from '../components/care/Care'
+import { Outlet } from 'react-router-dom'
 import style from './Layout.module.css'
-import Guarantee from '../pages/Guarantee'
-import Gift from '../pages/Gift'
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <div className={style.container}>
-       <Outlet />
+        <Outlet />
       </div>
       <Footer />
     </div>
