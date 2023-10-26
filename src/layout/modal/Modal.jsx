5615697ec221bad72e521dff491215841647905c
unknown <ulanuulunursultan36@gmail.com>
2023-10-26 11:35:20 +0600

    smail

--- src/layout/modal/Modal.jsx
+++ src/layout/modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import smaile1 from "../../assets/img/modal/sm.svg";
 import smaile2 from "../../assets/img/modal/sm2.svg";
 import whats from "../../assets/img/modal/ws.png";
@@ -9,57 +9,67 @@ import style from "./Modal.module.css";
 const Modal = () => {
   const [showFirstContent, setShowFirstContent] = useState(true);
   const [showText, setShowText] = useState(true);
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowComponent(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div>
-      <div
-        className={`${style.image_container} ${
-          showFirstContent ? style.showContent : style.hideContent
-        }`}
-        onClick={() => setShowFirstContent(!showFirstContent)}
-      >
-        <div className={style.contacts}>
-          <img src={x} alt="WhatsApp" width={50} />
+    showComponent && (
+      <div>
+        <div
+          className={`${style.image_container} ${
+            showFirstContent ? style.showContent : style.hideContent
+          }`}
+          onClick={() => setShowFirstContent(!showFirstContent)}
+        >
+          <div className={style.contacts}>
+            <img src={x} alt="WhatsApp" width={50} />
 
-          <a
-            href="https://wa.me/0502102270"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={whats} alt="WhatsApp" width={50} />
-          </a>
-          <a
-            href="https://t.me/Nur6ultan"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={tg} alt="Telegram" width={50} />
-          </a>
-          <a href="tel:+996502102270">
-            <img src={ph} alt="Phone" width={50} />
-          </a>
-        </div>
-
-        {showText && (
-          <div className={style.text}>
-            <button
-              className={style.closeButton}
-              onClick={() => setShowText(false)}
+            <a
+              href="https://wa.me/0502102270"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              &times;
-            </button>
-            <h3>Есть вопросы?</h3>
-            <p>напишите, поможем</p>
+              <img src={whats} alt="WhatsApp" width={50} />
+            </a>
+            <a
+              href="https://t.me/Nur6ultan"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={tg} alt="Telegram" width={50} />
+            </a>
+            <a href="tel:+996502102270">
+              <img src={ph} alt="Phone" width={50} />
+            </a>
           </div>
-        )}
 
-        <img
-          src={showFirstContent ? smaile2 : smaile1}
-          alt="Smiley"
-          className="visible"
-        />
+          {showText && (
+            <div className={style.text}>
+              <button
+                className={style.closeButton}
+                onClick={() => setShowText(false)}
+              >
+                &times;
+              </button>
+              <h3>Есть вопросы?</h3>
+              <p>напишите, поможем</p>
+            </div>
+          )}
+
+          <img
+            src={showFirstContent ? smaile2 : smaile1}
+            alt="Smiley"
+            className="visible"
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
